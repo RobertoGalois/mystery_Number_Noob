8bb712fd967d2d036c37643941d60225d3a43e69
RobertoGalois <victor.vague@live.fr>
2017-07-24 13:25:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,60 @@
-  1 #include    <stdio.h>
-  2 #include    <stdlib.h>
-  3 #include    <time.h>
-  4 #define     MIN 1
-  5 #define     MAX 100
-  6 
-  7 
-  8 void    verif_choice(signed short p_player_choice, signed short p_number)
-  9 {
- 10     if (p_player_choice > p_number)
- 11         printf("Le nombre choisi est trop GRAND !\n");
- 12 
- 13     else if (p_player_choice < p_number)
- 14         printf("le nombre choisi est trop PETIT !\n");
- 15 
- 16     else
- 17         printf("OH!\n");
- 18 }
- 19 
- 20 int     gen_number()
- 21 {
- 22     srand(time(NULL));
- 23     return (rand() % (MAX - MIN + 1)) + MIN;
- 24 }
- 25 
- 26 int     main(void)
- 27 {
- 28     signed short    number;
- 29     signed short    player_choice;
- 30     signed int      count;
- 31     char            *plural;
- 32 
- 33     plural = "s";
- 34     count = 0;
- 35     number = gen_number();
- 36     printf("Bienvenue dans le jeu du PLUS OU MOINS !\n");
- 37     printf("========================================\n");
- 38     while(player_choice != number)
- 39     {
- 40         count++;
- 41         printf("Entrez un nombre: ");
- 42         scanf("%hd", &player_choice);
- 43         getchar();
- 44         verif_choice(player_choice, number);
- 45     }
- 46 
- 47     if(count == 1)
- 48         plural = "";
- 49     
- 50     printf("Bravo ! Vous avez trouvé ! Le nombre mystere etait %d ! Vous avez trouve en %d essai%s !\n", number, count, plural);
- 51     return (0);
- 52 }
+#include	<stdio.h> 
+#include	<stdlib.h>
+#include	<time.h>
+#define		MIN 1
+#define		MAX 100
+
+/*
+** Say to the player that the number he chose is bigger or smaller (or same) as the "nombre mystere"
+*/
+void	verif_choice(signed short p_player_choice, signed short p_number)
+{
+	if (p_player_choice > p_number)
+		printf("Le nombre choisi est trop GRAND !\n");
+
+	else if (p_player_choice < p_number)
+		printf("le nombre choisi est trop PETIT !\n");
+
+	else 
+		printf("OH!\n");
+}
+
+/*
+** Generate a random integer between MIN and MAX
+*/
+int 	gen_number()
+{
+	srand(time(NULL));
+	return (rand() % (MAX - MIN + 1)) + MIN;
+}
+
+/*
+** Main..	
+*/
+int		main(void)
+{
+	signed short	number;
+	signed short	player_choice;
+	signed int		count;
+	char			*plural;
+
+	plural = "s";
+	count = 0;
+	number = gen_number();
+	printf("Bienvenue dans le jeu du PLUS OU MOINS !\n");
+	printf("========================================\n");
+	while(player_choice != number)
+	{
+		count++;
+		printf("Entrez un nombre: ");
+		scanf("%hd", &player_choice);
+		getchar();
+		verif_choice(player_choice, number);
+	}
+
+	if(count == 1)
+		plural = "";
+
+	printf("Bravo ! Vous avez trouvé ! Le nombre mystere etait %d ! Vous avez trouve en %d essai%s !\n", number, count, plural);
+	return (0);
+}
